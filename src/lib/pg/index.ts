ca6b73d2abeca27d749c2b26270cca14a8e4fdc4
Anton <steel.kom@gmail.com>
2023-03-07 16:31:56 +0300

    fix: rename exports

--- src/lib/pg/index.ts
+++ src/lib/pg/index.ts
@@ -1,4 +1,4 @@
-export * as pools from "./connection.js";
+export * as connection from "./connection.js";
 export * as DomainTypes from "./domain/types.js";
 export * as ModelTypes from "./model/types.js";
 export { BaseDomain } from "./domain/domain.js";
